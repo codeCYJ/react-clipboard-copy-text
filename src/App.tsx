2ad3a93f6b38c8a9faac624bcce0f13b68346cbb
codeCYJ <67721584+codeCYJ@users.noreply.github.com>
2022-11-11 12:08:29 +0900

    clipoboard

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import styled from "styled-components";
+
+function App() {
+  return <Container>
+    
+  </Container>;
+}
+
+const Container = styled.div``;
+export default App;
